43b997c0ee20060e4e1614fd23192d417c4ecd04
YWNlcg <cXdlcnR5@yandex.ru>
2021-09-11 16:58:19 +0300

    Added default information to the VPN connection creation window

--- src/NewVpnConnectionDialog.cpp
+++ src/NewVpnConnectionDialog.cpp
@@ -12,7 +12,26 @@ NewVpnConnectionDialog::~NewVpnConnectionDialog() {
 }
 
 void NewVpnConnectionDialog::init() {
+    // Default Setting Name
+    QString defName = "New Vpn Connection";
+    QString pattern = defName + " (%1)";
+    for (int i = 2; !checkSettingName(defName) && i != INT_MAX; ++i) {
+        defName = pattern.arg(i);
+    }
+    _ui->labelSettingName->setText(defName);
 
+    // Default ports
+    _ui->comboBoxPortNumber->addItem("8888");
+    _ui->comboBoxPortNumber->addItem("443");
+    _ui->comboBoxPortNumber->addItem("992");
+    _ui->comboBoxPortNumber->addItem("5555");
+
+    // Default Auth Type
+    _ui->comboBoxAuthType->addItem("Anonymous Authentication");
+    _ui->comboBoxAuthType->addItem("Standart PAssword Authentication");
+    _ui->comboBoxAuthType->addItem("RADIUS or NT Domain Authentication");
+    _ui->comboBoxAuthType->addItem("Client Certificate Authentication");
+    _ui->comboBoxAuthType->addItem("Smart Card Authentication");
 }
 
 bool NewVpnConnectionDialog::checkSettingName(const QString &name) {
